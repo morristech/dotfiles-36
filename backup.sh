473a63c17d0d4cd6960b752d99715bd5a8553297
David Pursehouse <david.pursehouse@gmail.com>
2013-10-02 10:30:31 +0900

    Add gzip compression on backup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tar -c $1 | \
+tar -c $1 | gzip | \
     gpg --encrypt --recipient 'david.pursehouse@gmail.com' |  \
-    split -b 10M -a 4 -d - $1.tar.gpg.
+    split -b 10M -a 4 -d - $1.tar.gz.gpg.
 
