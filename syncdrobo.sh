bf4a36b21daee9841f34a51096b8f2621616e2c4
David Pursehouse <david.pursehouse@gmail.com>
2013-12-06 23:31:26 +0900

    Fix Documents path
    Change-Id: I45611e1b14ce3d713c65a8f50ab6a8016eb1d6db

--- syncdrobo.sh
+++ syncdrobo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/backup/Documents root@192.168.1.99:/mnt/DroboFS/Shares/share
+rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Documents root@192.168.1.99:/mnt/DroboFS/Shares/share
 
