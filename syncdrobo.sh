f040346c6475ab14393adfe769d78325b13906bb
David Pursehouse <david.pursehouse@gmail.com>
2014-02-13 10:18:36 +0900

    Add photos folder to Drobo sync
    Change-Id: I66d282704c2cbc6193f20c3cb5a72044e6e661e5

--- syncdrobo.sh
+++ syncdrobo.sh
@@ -2,3 +2,4 @@
 
 rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Documents root@192.168.1.99:/mnt/DroboFS/Shares/share
 
+rsync -e "ssh -c arcfour" --exclude "Previews*" --exclude "Thumbnails*" -raP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Pictures/* root@192.168.1.99:/mnt/DroboFS/Shares/share/Photos/
