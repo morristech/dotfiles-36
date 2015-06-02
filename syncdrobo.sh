ab4e44234337cf4ebf7bf61d6d022ad00d149403
David Pursehouse <david.pursehouse@gmail.com>
2015-06-03 00:37:22 +0900

    Sync camcorder files to Drobo
    Change-Id: I4b9ce787e57b762e98ba96018e421f0225fbe2fe

--- syncdrobo.sh
+++ syncdrobo.sh
@@ -3,3 +3,6 @@
 rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Documents root@192.168.1.99:/mnt/DroboFS/Shares/share
 
 rsync -e "ssh -c arcfour" --exclude "Previews*" --exclude "Thumbnails*" -raP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Pictures/* root@192.168.1.99:/mnt/DroboFS/Shares/share/Photos/
+
+rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Movies/HomeMovies/ root@192.168.1.99:/mnt/DroboFS/Shares/share/HomeMovies/
+
