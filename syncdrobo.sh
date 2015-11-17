e8df3a5271c967d0dc7aa56a3fb9e55deb8db310
David Pursehouse <david.pursehouse@gmail.com>
2015-11-17 23:17:30 +0900

    Don't sync to Drobo as root
    Change-Id: I1ccdceba11fa2ea5e30e7f13d5d57c5fe650d553

--- syncdrobo.sh
+++ syncdrobo.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Documents root@192.168.1.99:/mnt/DroboFS/Shares/share
+rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Documents Admin@192.168.1.99:/mnt/DroboFS/Shares/share
 
-rsync -e "ssh -c arcfour" -raP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Pictures/* root@192.168.1.99:/mnt/DroboFS/Shares/share/Photos/
+rsync -e "ssh -c arcfour" -raP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Pictures/* Admin@192.168.1.99:/mnt/DroboFS/Shares/share/Photos/
 
-rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Movies/HomeMovies/ root@192.168.1.99:/mnt/DroboFS/Shares/share/HomeMovies/
+rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Movies/HomeMovies/ Admin@192.168.1.99:/mnt/DroboFS/Shares/share/HomeMovies/
 
