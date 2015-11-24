c7436bed8919f6b950b7e646015daf6e74458563
David Pursehouse <david.pursehouse@gmail.com>
2015-11-24 22:41:32 +0900

    Fix 'no matching cipher found' when synching photos
    Change-Id: If651edee6c88b7541ff5237724e7e1729fc677a0

--- syncdrobo.sh
+++ syncdrobo.sh
@@ -2,7 +2,7 @@
 
 rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Documents Admin@192.168.1.99:/mnt/DroboFS/Shares/share
 
-rsync -e "ssh -c arcfour" -raP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Pictures/* Admin@192.168.1.99:/mnt/DroboFS/Shares/share/Photos/
+rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Pictures/* Admin@192.168.1.99:/mnt/DroboFS/Shares/share/Photos/
 
 rsync -ravzP --rsync-path /mnt/DroboFS/Shares/DroboApps/rsync/bin/rsync ~/Movies/HomeMovies/ Admin@192.168.1.99:/mnt/DroboFS/Shares/share/HomeMovies/
 
